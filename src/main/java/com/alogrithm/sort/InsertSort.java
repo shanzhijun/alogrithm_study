760ecd5c101e1c42476fec1530c05bb0ce49c983
shanzhijun <you@example.com>
2018-09-29 18:00:44 +0800

    插入排序

---
+++ src/main/java/com/alogrithm/sort/InsertSort.java
@@ -0,0 +1,28 @@
+package com.alogrithm.sort;
+
+import org.junit.Test;
+
+/**
+ * 插入排序
+ */
+public class InsertSort extends BaseSort {
+
+    @Test
+    public void insertSort(){
+        int[] arr={13,1,2,14,5,3,1,39,4,22,11,19};
+
+        for (int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    arr[j-1]=arr[j-1]^arr[j];
+                    arr[j]=arr[j-1]^arr[j];
+                    arr[j-1]=arr[j-1]^arr[j];
+                }else{
+                    break;
+                }
+            }
+        }
+
+        soutSortVlaue(arr);
+    }
+}
